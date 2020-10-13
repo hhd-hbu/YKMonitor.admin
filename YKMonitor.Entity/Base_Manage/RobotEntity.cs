0211b60a0282f9a1801af8acc63dacbcb440e8a6
HanYihao <hyh991031@163.com>
2020-10-13 16:38:25 +0800

    robotentity2020/10/13

--- YKMonitor.Entity/Base_Manage/RobotEntity.cs
+++ YKMonitor.Entity/Base_Manage/RobotEntity.cs
@@ -8,6 +8,37 @@ namespace YKMonitor.Entity.Base_Manage
     [Table("TK_Robot")]
     public class RobotEntity:BaseEntity
     {
-        public int MyProperty { get; set; }
+        /// <summary>
+        /// 机器人名称
+        /// </summary>
+        public string robot_name { get; set; }
+        /// <summary>
+        /// 机器人链接
+        /// </summary>
+        public string link { get; set; }
+        /// <summary>
+        /// 短信
+        /// </summary>
+        public string shortmessage { get; set; }
+        /// <summary>
+        /// 邮箱
+        /// </summary>
+        public string email { get; set; }
+        /// <summary>
+        /// 创建人id
+        /// </summary>
+        public string founder_id { get; set; }
+        /// <summary>
+        /// 修改人id
+        /// </summary>
+        public string modifier_id { get; set; }
+        /// <summary>
+        /// 修改时间
+        /// </summary>
+        public DateTime modifier_time { get; set; }
+        /// <summary>
+        /// 备注
+        /// </summary>
+        public string remarks { get; set; }
     }
 }
